3a8a41ab5fb5ac87d37f2a299877f4e1431a831c
WillemBarendKruger <willem.kruger11@gmail.com>
2025-05-24 20:39:41 +0200

    bug - fixed the submistion on empty values in the inputs

--- js/register.js
+++ js/register.js
@@ -1,34 +1,41 @@
 const registerUser = () => {
     const username = document.getElementById("userName").value.toUpperCase().trim();
     const password = document.getElementById("passWord").value;
+    const login = document.getElementById("login");
+    const formSwitch = document.getElementById("successRegister")
 
-    // Check if the username already exists
     const users = JSON.parse(localStorage.getItem('users')) || [];
     for (let i = 0; i < users.length; i++) {
         if (users[i].Username === username) {
             const wrong = document.getElementById("heading");
             wrong.innerHTML = "Username already taken";
-            document.getElementById("userName").value = "";
-            document.getElementById("passWord").value = "";
+            username.value = "";
+            password.value = "";
             return;
         }
     }
 
-    const login = document.getElementById("login").style.display = "none";;
-    const sucess = document.getElementById("successRegister").style.display = "flex";
-    document.getElementById("userName").value = "";
-    document.getElementById("passWord").value = "";
+    if (!username || !password) {
+        document.getElementById("successMessage").innerText = "Please fill all the fields";
+        document.querySelector("#retry").setAttribute("href", "./register.html");
+        login.style.display = "none";
+        formSwitch.style.display = "flex";
+        return;
+    }
 
-    // Hash the password using Web Crypto API
+    login.style.display = "none";
+    formSwitch.style.display = "flex";
+
     window.crypto.subtle.digest("SHA-256", new TextEncoder().encode(password))
         .then((hashedPassword) => {
             let hashArray = Array.from(new Uint8Array(hashedPassword));
             let hashHex = hashArray.map(passw => ('00' + passw.toString(16)).slice(-2)).join('');
 
-            // Store the user data in local storage
             let userData = { Username: username, Password: hashHex, user_Active: Date.now() };
             users.push(userData);
             localStorage.setItem('users', JSON.stringify(users));
+            username.value = "";
+            password.value = "";
         })
         .catch((error) => {
             console.error("Error hashing password: ", error);
